0fcb2c4cd6b02c8f3e9b802ff9394fa0fdede2b6
Contra <contra@maricopa.edu>
2012-06-28 18:09:39 -0700

    fix boards nav on thread view

--- client/js/routes/thread.coffee
+++ client/js/routes/thread.coffee
@@ -1,24 +1,26 @@
 define ["chan/server", "chan/notify", "templates/board", "templates/reply", "templates/post"], (server, notify, board, reply, postTempl) ->
   ({id, post}, templ) ->
     server.ready ->
-      server.thread id, (err, thread) ->
-        return notify.error "Error grabbing thread: #{err}" if err?
-        $("#content").html board board: thread.board unless $("#threadview")[0]
-        $('#reply').html reply type: 'thread', id: id
-        $("#replyForm").ajaxForm 
-          dataType: 'json'
-          error: (req, err, erra) -> notify.error req.responseText
-        $("#threadview").html templ thread
-        #$('.lazy').jail effect: 'fadeIn'
-        if post?
-          $('html,body').animate
-            scrollTop: $("##{post._id}").offset()?.top
-          , 1000
+      server.boards (err, boards) ->
+        return notify.error "Error grabbing boards: #{err}" if err?
+        server.thread id, (err, thread) ->
+          return notify.error "Error grabbing thread: #{err}" if err?
+          $("#content").html board board: thread.board, boards: boards unless $("#threadview")[0]
+          $('#reply').html reply type: 'thread', id: id
+          $("#replyForm").ajaxForm 
+            dataType: 'json'
+            error: (req, err, erra) -> notify.error req.responseText
+          $("#threadview").html templ thread
+          #$('.lazy').jail effect: 'fadeIn'
+          if post?
+            $('html,body').animate
+              scrollTop: $("##{post._id}").offset()?.top
+            , 1000
 
-        server.subscribe.newPost (nthread, post) ->
-          return unless nthread._id is thread._id
-          nthread.post = post
-          $("##{nthread._id}").append postTempl nthread
-          $('html,body').animate
-            scrollTop: $("##{post._id}").offset()?.top
-          , 1000
+          server.subscribe.newPost (nthread, post) ->
+            return unless nthread._id is thread._id
+            nthread.post = post
+            $("##{nthread._id}").append postTempl nthread
+            $('html,body').animate
+              scrollTop: $("##{post._id}").offset()?.top
+            , 1000
--- public/js/routes/thread.js
+++ public/js/routes/thread.js
@@ -6,42 +6,48 @@
       var id, post;
       id = _arg.id, post = _arg.post;
       return server.ready(function() {
-        return server.thread(id, function(err, thread) {
-          var _ref;
+        return server.boards(function(err, boards) {
           if (err != null) {
-            return notify.error("Error grabbing thread: " + err);
+            return notify.error("Error grabbing boards: " + err);
           }
-          if (!$("#threadview")[0]) {
-            $("#content").html(board({
-              board: thread.board
+          return server.thread(id, function(err, thread) {
+            var _ref;
+            if (err != null) {
+              return notify.error("Error grabbing thread: " + err);
+            }
+            if (!$("#threadview")[0]) {
+              $("#content").html(board({
+                board: thread.board,
+                boards: boards
+              }));
+            }
+            $('#reply').html(reply({
+              type: 'thread',
+              id: id
             }));
-          }
-          $('#reply').html(reply({
-            type: 'thread',
-            id: id
-          }));
-          $("#replyForm").ajaxForm({
-            dataType: 'json',
-            error: function(req, err, erra) {
-              return notify.error(req.responseText);
+            $("#replyForm").ajaxForm({
+              dataType: 'json',
+              error: function(req, err, erra) {
+                return notify.error(req.responseText);
+              }
+            });
+            $("#threadview").html(templ(thread));
+            if (post != null) {
+              $('html,body').animate({
+                scrollTop: (_ref = $("#" + post._id).offset()) != null ? _ref.top : void 0
+              }, 1000);
             }
-          });
-          $("#threadview").html(templ(thread));
-          if (post != null) {
-            $('html,body').animate({
-              scrollTop: (_ref = $("#" + post._id).offset()) != null ? _ref.top : void 0
-            }, 1000);
-          }
-          return server.subscribe.newPost(function(nthread, post) {
-            var _ref1;
-            if (nthread._id !== thread._id) {
-              return;
-            }
-            nthread.post = post;
-            $("#" + nthread._id).append(postTempl(nthread));
-            return $('html,body').animate({
-              scrollTop: (_ref1 = $("#" + post._id).offset()) != null ? _ref1.top : void 0
-            }, 1000);
+            return server.subscribe.newPost(function(nthread, post) {
+              var _ref1;
+              if (nthread._id !== thread._id) {
+                return;
+              }
+              nthread.post = post;
+              $("#" + nthread._id).append(postTempl(nthread));
+              return $('html,body').animate({
+                scrollTop: (_ref1 = $("#" + post._id).offset()) != null ? _ref1.top : void 0
+              }, 1000);
+            });
           });
         });
       });
